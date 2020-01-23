de2108586f37bca83ca9371337d50ed113c64d3b
Ali Fathi <alifathi1388@yahoo.com>
2020-01-24 00:39:01 +0330

    create Thumbnaul extension method for UrlHelper.

---
+++ Parnian/App_Start/MvcHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Parnian
+{
+    public static class MvcHelperExtensions
+    {
+        public static string Thumbnail(this UrlHelper url, string path, int width)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return "/App_Files/UI/no-img.jpg";
+
+            return url.Action("Thumbnail", "FileManager", new { area = "Kaveh", path, width });
+        }
+    }
+}
