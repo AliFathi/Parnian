10ebaec74a6a8f3079482b53da5cf96ec3bcf876
Ali Fathi <alifathi1388@yahoo.com>
2020-01-24 00:00:55 +0330

    add FileHasher

---
+++ Parnian/App_Start/FileHasher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Parnian
+{
+    public static class FileHasher
+    {
+        public static string CalculateMD5(string filename)
+        {
+            if (filename == null)
+                throw new ArgumentNullException(nameof(filename));
+
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream))
+                        .Replace("-", "")
+                        .ToLowerInvariant();
+                }
+            }
+        }
+    }
+}
